ae3c3a1b6b9d864af1c69a229cd153b73a0179e9
eogito <106759344+eogito@users.noreply.github.com>
2023-07-28 22:18:32 -0400

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+    "manifest_version": 3,
+    "name": "Extension",
+    "version": "1.0",
+    "description": "amogus",
+    "background": {
+        "service_worker": "background.js"
+      },
+    "action": {
+        "default_popup": "index.html"
+    },
+    "permissions": [
+        "tabs"
+    ]
+}
